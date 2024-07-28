4b2e573de0531224c56eb6d9f24a8232fdf28026
Vitaliy <gusevvnn@gmail.com>
2024-07-28 20:04:42 +0500

    Added and connected store redux

---
+++ src/store/treeSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const treeSlice = createSlice({
+  name: "tree",
+  initialState: {
+    programs: [],
+  },
+  reducers: {},
+});
+
+export const {} = treeSlice.actions;
+export default treeSlice.reducer;
