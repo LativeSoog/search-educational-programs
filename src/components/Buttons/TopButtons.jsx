fd21e83e60ade0c24a2c2f958ed45ee5bfc1cb28
Vitaliy <gusevvnn@gmail.com>
2024-09-06 01:49:09 +0500

    Updated the layout of the toolbar

--- src/components/Buttons/TopButtons.jsx
+++ src/components/Buttons/TopButtons.jsx
@@ -2,15 +2,14 @@ import S from "./TopButtons.module.scss";
 
 export const TopButtons = ({ setIsShowFilter }) => {
   return (
-    <div className={S["content"]}>
+    <div className={S["toolbar"]}>
       <button
-        className={S["content__button"]}
+        className={S["toolbar__button"]}
         onClick={() =>
           setIsShowFilter((prevState) => ({ ...prevState, EGE: true }))
         }>
-        Калькулятор ЕГЭ
+        Подобрать программу по предметам ЕГЭ
       </button>
-      <button className={S["content__button"]}>Раскрыть все программы</button>
     </div>
   );
 };
