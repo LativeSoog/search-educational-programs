9dd732aa85ca3c074990308cd3ff1c5fc3eb8d96
Vitaliy <gusevvnn@gmail.com>
2024-09-06 01:34:08 +0500

    The layout of the component of educational programs has been changed

--- src/components/Spoilers/SpoilerProgram.jsx
+++ src/components/Spoilers/SpoilerProgram.jsx
@@ -5,64 +5,77 @@ import mappingData from "../../assets/data/mappingData.json";
 
 export const SpoilerProgram = ({ program }) => {
   const [isShowMoreInfo, setIsShowMoreInfo] = useState(false);
+  const [specializationCode, specializationName] =
+    program.nameSpecialization.split(" - ");
 
   const handleClickShowMore = () => {
     setIsShowMoreInfo(!isShowMoreInfo);
   };
 
+  console.log(program);
+
   return (
-    <div className={S["spoiler"]}>
-      <div className={S["spoiler-visible"]}>
-        <div className={S["spoiler-program"]}>
-          <div className={S["spoiler-program__specialization"]}>
-            <p className={S["spoiler-program__description"]}>
-              Направление подготовки
-            </p>
-            <p className={S["spoiler-program__value"]}>
-              {program.nameSpecialization}
-            </p>
+    <div className={S["program"]}>
+      <div className={S["program-visible"]}>
+        <div className={S["program-head"]}>
+          <h3 className={S["program-head__name"]}>{program.nameProgram}</h3>
+          <div className={S["program-head__specialization"]}>
+            <span className={S["program-head__spec-code"]}>
+              {specializationCode}
+            </span>
+            <span className={S["program-head__spec-name"]}>
+              {specializationName}
+            </span>
           </div>
-          <div className={S["spoiler-program__name"]}>
-            <p className={S["spoiler-program__description"]}>
-              Программа (профиль)
-            </p>
-            <p className={S["spoiler-program__value"]}>{program.nameProgram}</p>
-          </div>
-          <div className={S["spoiler-program__form"]}>
-            <p className={S["spoiler-program__description"]}>Форма обучения</p>
-            <p className={S["spoiler-program__value"]}>
+        </div>
+        <div className={S["program-body"]}>
+          <div className={S["program-body__specificity"]}>
+            <div className={S["program-body__item"]}>
+              Базовое высшее образование
+            </div>
+            <div className={S["program-body__item"]}>
               {mappingData.formMapping[program.form]}
-            </p>
+            </div>
+            <a
+              href=""
+              className={S["program-body__button"]}>
+              Описание программы &raquo;
+            </a>
           </div>
+        </div>
+        <div className={S["program-bottom"]}>
+          <button
+            className={S["program-bottom__more"]}
+            onClick={() => handleClickShowMore()}>
+            {isShowMoreInfo ? "Скрыть" : "Показать"} подробную информацию
+          </button>
         </div>
       </div>
       {isShowMoreInfo && (
-        <div className={S["spoiler-hidden"]}>
-          <div className={S["spoiler-info"]}>
-            <div className={S["spoiler-info__item"]}>
-              <p className={S["spoiler-info__description"]}>Бюджетные места</p>
-              <p className={S["spoiler-info__value"]}>{program.kcp}</p>
+        <div className={S["program-hidden"]}>
+          <div className={S["program-info"]}>
+            <div className={S["program-info__item"]}>
+              <h4 className={S["program-info__name"]}>Бюджетные места</h4>
+              <p className={S["program-info__value"]}>{program.kcp}</p>
             </div>
-            <div className={S["spoiler-info__item"]}>
-              <p className={S["spoiler-info__description"]}>Срок обучения</p>
-              <p className={S["spoiler-info__value"]}>{program.duration}</p>
-            </div>
-            <div className={S["spoiler-info__item"]}>
-              <p className={S["spoiler-info__description"]}>
-                Проходной балл в 2023 году
+            <div className={S["program-info__item"]}>
+              <h4 className={S["program-info__name"]}>Форма обучения</h4>
+              <p className={S["program-info__value"]}>
+                {mappingData.formMapping[program.form]}
               </p>
-              <p className={S["spoiler-info__value"]}>{program.score}</p>
             </div>
-            <div className={S["spoiler-info__item"]}>
-              <p className={S["spoiler-info__description"]}>
-                Стоимость обучения (в год)
-              </p>
-              <p className={S["spoiler-info__value"]}>
+            <div className={S["program-info__item"]}>
+              <h4 className={S["program-info__name"]}>Проходной балл</h4>
+              <p className={S["program-info__value"]}>{program.score}</p>
+            </div>
+            <div className={S["program-info__item"]}>
+              <h4 className={S["program-info__name"]}>Стоимость обучения</h4>
+              <p className={S["program-info__value"]}>
                 {program.price} &#8381;
               </p>
             </div>
           </div>
-          <div className={S["spoiler-exams"]}>
+          <div className={S["program-exams"]}>
             <ExamsList
               nameList={"Вступительные испытания (ЕГЭ)"}
               examsList={program.examsEGE}
@@ -74,16 +87,6 @@ export const SpoilerProgram = ({ program }) => {
           </div>
         </div>
       )}
-
-      <div
-        className={S["spoiler-more"]}
-        onClick={() => handleClickShowMore()}>
-        <div className={S["spoiler-more__btn"]}>
-          {isShowMoreInfo
-            ? "Скрыть подробную информацию"
-            : "Показать подробную информацию"}
-        </div>
-      </div>
     </div>
   );
 };
