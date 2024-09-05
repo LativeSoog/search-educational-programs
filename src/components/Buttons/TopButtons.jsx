dabb649072fe035e8170cca11ccd35493db64849
Vitaliy <gusevvnn@gmail.com>
2024-09-06 02:36:33 +0500

    Added the ability to clear filters, fixed styles

--- src/components/Buttons/TopButtons.jsx
+++ src/components/Buttons/TopButtons.jsx
@@ -1,6 +1,15 @@
+import { useDispatch, useSelector } from "react-redux";
 import S from "./TopButtons.module.scss";
+import { changeFilters } from "../../store/treeSlice";
 
 export const TopButtons = ({ setIsShowFilter }) => {
+  const dispatch = useDispatch();
+  const isFiltered = useSelector((state) => state.tree.filteredListPrograms);
+
+  const handleClearFilters = () => {
+    dispatch(changeFilters(false));
+  };
+
   return (
     <div className={S["toolbar"]}>
       <button
@@ -10,6 +19,13 @@ export const TopButtons = ({ setIsShowFilter }) => {
         }>
         Подобрать программу по предметам ЕГЭ
       </button>
+      {isFiltered && (
+        <button
+          className={S["toolbar__button_filter"]}
+          onClick={() => handleClearFilters()}>
+          Очистить фильтры
+        </button>
+      )}
     </div>
   );
 };
